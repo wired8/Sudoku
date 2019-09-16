7a09c221bdfea097056edfae9685f311721c2741
PhilJ <phil@wired8.com>
2019-09-15 20:13:58 -0700

    Added production build

--- test/board.js
+++ test/board.js
@@ -4,7 +4,6 @@ const Solver = require('../lib/solver');
 const solver = new Solver(50000);
 const suduko = new Suduko(27);
 
-//const puzzle = [ [1,2,3,4,5,6,7,8,9], [2,3,4,5,6,7,8,9,1], [9,5,4,3,2,8,7,6,1], [1,2,3,4,5,6,7,8,9], '', '', '', '', '' ];
 const puzzle = suduko.getBoard();
 
 console.log('Puzzle', puzzle);
